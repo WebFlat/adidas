b4ab2623ee0882b48dca49aed169b0f4a4339f17
WebFlat <y.vacheslav@gmail.com>
2019-11-08 00:29:07 +0300

    complete search & slider

--- js/script.js
+++ js/script.js
@@ -18,16 +18,28 @@ $(document).ready(function() {
     }
  });
 
-
+//Show nav-bar***************************************************************************
  let g_top = 0;
   $(window).scroll(function() {
     let top = $(this).scrollTop();
     if ( top < g_top ) {
-      $('header').css("position", "fixed").css("z-index", "200").css("transform", "translateY(0)");
+      $('.header').css("position", "fixed").css("z-index", "20").css("transform", "translateY(0)");
     } else {
-      $('header').css("position", "absolute").css("transform", "translateY(-100px)");
+      $('.header').css("position", "absolute").css("transform", "translateY(-100px)");
     }
     g_top = top;    
   });
+
+
+//Open-Close search*****************************************************************************
+  $('.nav-bottom__search').on('click', function(e) {
+		e.preventDefault;
+		$('.nav-search').css("transform", "translateY(0)").css("position", "fixed");
+
+	});
+  $('.nav-search__close').on('click', function(e) {
+    e.preventDefault;
+    $('.nav-search').css("transform", "translateY(-800px)");
+  })
 
 });
